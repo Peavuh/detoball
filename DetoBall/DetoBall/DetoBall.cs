39e56bcd1919b45b17e0b9f29f719d1894c46237
Peavuh <aapo.jussila09@gmail.com>
2023-06-13 13:36:45 +0300

    ball go boom haha

--- DetoBall/DetoBall/DetoBall.cs
+++ DetoBall/DetoBall/DetoBall.cs
@@ -12,103 +12,150 @@ public class DetoBall : PhysicsGame
     private PhysicsObject ball;
     public override void Begin()
     {
-        //make the ball explode yes yes
+        Keyboard.Listen(Key.L, ButtonState.Pressed, boom, "explode, but left");
+        Gravity = new Vector(0.0, -981.0);
+        Keyboard.Listen(Key.J, ButtonState.Pressed, kaboom, "explode, but right");
         LuoKentta();
-
-        
+        Keyboard.Listen(Key.D, ButtonState.Down, right, null);
+        Keyboard.Listen(Key.A, ButtonState.Down, left, null);
         Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "end game");
     }
     
     
     void LuoKentta()
     {
-        ball = new PhysicsObject(40.0, 40.0);
+        ball = new PhysicsObject(25.0, 25.0);
         ball.Shape = Shape.Circle;
         ball.X = -200.0;
         ball.Y = -50;
-        ball.Restitution = 1.0;
+        ball.Restitution = 0.5;
         Add(ball);
-
+        
         Level.CreateBorders(1.0, false);
         Level.Background.Color = Color.Black;
 
         Camera.ZoomToLevel();
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
+
+
+
+
+    void kaboom()
+    { 
+        
+        Explosion rajahdys = new Explosion(50);
+        rajahdys.Position = ball.Position;
+        Add(rajahdys);
+
+        rajahdys.Speed = 500.0;
+        rajahdys.Force = 15.0;
+        ball.IgnoresExplosions = true;
+        ball.Hit(new Vector(500, 450));
+
+
+
+
+
+    }
+
+    void right()
+    {
+        ball.Push(new Vector(1000, 0));
+        
+        
+        
+        
+    }
+
+    void left()
+    {
+        ball.Push(new Vector(-1000, 0));
+        
+        
+        
+        
+    }
+
+    void boom()
+    {
+        Explosion rajahdys = new Explosion(50);
+        rajahdys.Position = ball.Position;
+        Add(rajahdys);
+
+        rajahdys.Speed = 500.0;
+        rajahdys.Force = 15.0;
+        ball.IgnoresExplosions = true;
+        ball.Hit(new Vector(-500, 450));
+        
+        
+        
+        
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
