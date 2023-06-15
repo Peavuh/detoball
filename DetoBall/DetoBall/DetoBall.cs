4b6dc181ab37514e787d2baf2924c7e1b3744aa0
Peavuh <aapo.jussila09@gmail.com>
2023-06-15 11:17:37 +0300

    it works, also all maps done and spikes done too

--- DetoBall/DetoBall/DetoBall.cs
+++ DetoBall/DetoBall/DetoBall.cs
@@ -19,14 +19,9 @@ public class DetoBall : PhysicsGame
     
     public override void Begin()
     {
-        Seuraavakenttä();
-        Keyboard.Listen(Key.J, ButtonState.Pressed, boom, "explode, but left");
-        Gravity = new Vector(0.0, -981.0);
-        Keyboard.Listen(Key.L, ButtonState.Pressed, kaboom, "explode, but right");
-        Keyboard.Listen(Key.D, ButtonState.Down, right, null);
-        Keyboard.Listen(Key.A, ButtonState.Down, left, null);
-        Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "end game");
-        Keyboard.Listen(Key.K, ButtonState.Pressed, kablooey, "explodes, but up");
+        bal_move();
+        Seuraavakentta();
+        
         
         
         
@@ -42,16 +37,17 @@ public class DetoBall : PhysicsGame
         ruudut.SetTileMethod('P',the_ballmaker);
         ruudut.SetTileMethod('X',LuoSeina, Color.Gray);
         ruudut.SetTileMethod('M',Stylish_Finish, Color.Cyan);
+        ruudut.SetTileMethod('Y', death_doritos, Color.Cyan);
         ruudut.Execute(42, 42);
         Camera.ZoomToLevel();
     }
 
-    void Seuraavakenttä()
+    void Seuraavakentta()
     {
         ClearAll();
-
+        bal_move();
         if (kenttaNro == 1) LuoKentta("kentta1.txt");
-        else if (kenttaNro == 2) LuoKentta("kentta2");
+        else if (kenttaNro == 2) LuoKentta("kentta2.txt");
         else if (kenttaNro == 3) LuoKentta("kentta3");
         else if (kenttaNro == 4) LuoKentta("kentta4");
         else if (kenttaNro == 5) LuoKentta("kentta5");
@@ -124,7 +120,7 @@ public class DetoBall : PhysicsGame
         rajahdys.Speed = 500.0;
         rajahdys.Force = 15.0;
         ball.IgnoresExplosions = true;
-        ball.Hit(new Vector(0, 1000));
+        ball.Hit(new Vector(0, 800));
         
         
         
@@ -156,26 +152,67 @@ public class DetoBall : PhysicsGame
         ball.Shape = Shape.Circle;
         ball.Position = paikka;
         ball.Restitution = 0.5;
-        
+        ball.Tag = "bal";
         Add(ball);
         
         
     }
 
-    void Stylish_Finish(Vector paikka, double leveys, double korkeus, Color väri)
+    void Stylish_Finish(Vector paikka, double leveys, double korkeus, Color vari)
     {
         PhysicsObject finish = new PhysicsObject(30.0, 30.0);
         finish.Shape = Shape.Rectangle;
         finish.IgnoresExplosions = true;
         finish.IgnoresGravity = true;
         finish.Restitution = 0;
-        Add(finish);
+        finish.Position = paikka;
+        finish.Color=Color.Cyan;
         finish.MakeStatic();
+        AddCollisionHandler(finish, "bal", TormasiMaaliin);
+        
+        Add(finish);
+        
     }
 
+    void death_doritos(Vector paikka, double leveys, double korkeus, Color vari)
+    {
+        PhysicsObject spike = new PhysicsObject(30.0, 30.0);
+        spike.Shape = Shape.Triangle;
+        spike.IgnoresExplosions = true;
+        spike.IgnoresGravity = true;
+        spike.Restitution = 0;
+        spike.Position = paikka;
+        spike.Color = Color.Red;
+        spike.MakeStatic();
+        AddCollisionHandler(spike, "bal", death);
+        Add(spike);
+    }
+    void TormasiMaaliin(PhysicsObject ball, PhysicsObject finish)
+    {
+        //Kasvatetaan kenttänumeroa yhdellä ja siirrytään seuraavaan kenttään:
+        kenttaNro++;
+        Seuraavakentta();
+    }
+    void death(PhysicsObject ball, PhysicsObject spike)
+    {
+        //Sama kenttä ladataan alusta jos kenttänumeroa ei kasvateta:
+        Seuraavakentta();
+        
+    }
 
-
-
+    void bal_move()
+    {
+        Keyboard.Listen(Key.J, ButtonState.Pressed, boom, "explode, but left");
+        Gravity = new Vector(0.0, -981.0);
+        Keyboard.Listen(Key.L, ButtonState.Pressed, kaboom, "explode, but right");
+        Keyboard.Listen(Key.D, ButtonState.Down, right, null);
+        Keyboard.Listen(Key.A, ButtonState.Down, left, null);
+        Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "end game");
+        Keyboard.Listen(Key.K, ButtonState.Pressed, kablooey, "explodes, but up");
+        
+        
+        
+    }
 
 
 
